77e2de67c7a72ec36c7b49ffcc5243810b490233
hss <hssha@qq.com>
2020-04-13 17:23:09 +0800

    Init

---
+++ src/main/java/com/bocsoft/vidie724/Vidie724Application.java
@@ -0,0 +1,13 @@
+package com.bocsoft.vidie724;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vidie724Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vidie724Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bocsoft/vidie724/Vidie724ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bocsoft.vidie724;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vidie724ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
